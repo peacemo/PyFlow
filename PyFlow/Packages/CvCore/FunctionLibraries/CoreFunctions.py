cd83ab3edda1e4fa564b4566dc0818c19196ab01
陈薛强 <carlchenxq@qq.com>
2025-07-08 17:03:54 +0800

    feat: support imread flags in loadImage2
    fix: improve loadImage2 function for better path handling

--- PyFlow/Packages/CvCore/FunctionLibraries/CoreFunctions.py
+++ PyFlow/Packages/CvCore/FunctionLibraries/CoreFunctions.py
@@ -1,21 +1,35 @@
 from PyFlow.Core.Common import *
 from PyFlow.Core import FunctionLibraryBase
 from PyFlow.Core import IMPLEMENT_NODE
+import numpy as np
 import cv2
-
+import os
 
 class CoreFunctions(FunctionLibraryBase):
-    '''doc string for DemoLib'''
+    '''doc string for CoreFunctions'''
 
     def __init__(self, packageName):
         super(CoreFunctions, self).__init__(packageName)
 
     @staticmethod
-    @IMPLEMENT_NODE(returns=None, meta={NodeMeta.CATEGORY: 'Core Functions', NodeMeta.KEYWORDS: []})
-    def loadImage2(path=('StringPin', "", {PinSpecifiers.INPUT_WIDGET_VARIANT: "FilePathWidget"}), 
+    @IMPLEMENT_NODE(returns=None, meta={NodeMeta.CATEGORY: 'I/O', NodeMeta.KEYWORDS: []})
+    def loadImage2(path=('StringPin', "", {PinSpecifiers.INPUT_WIDGET_VARIANT: "FilePathWidget"}),
+                   flag=('StringPin', 'IMREAD_COLOR',
+                            {PinSpecifiers.VALUE_LIST: \
+                             ['IMREAD_UNCHANGED', 'IMREAD_GRAYSCALE', 'IMREAD_COLOR', \
+                              'IMREAD_ANYDEPTH','IMREAD_ANYCOLOR','IMREAD_LOAD_GDAL', \
+                              'IMREAD_REDUCED_GRAYSCALE_2', 'IMREAD_REDUCED_COLOR_2', \
+                              'IMREAD_REDUCED_GRAYSCALE_4', 'IMREAD_REDUCED_COLOR_4', \
+                              'IMREAD_REDUCED_GRAYSCALE_8', 'IMREAD_REDUCED_COLOR_8', \
+                              'IMREAD_IGNORE_ORIENTATION ']}), 
                    img=(REF, ('AnyPin', None))):
-        if isinstance(path, str):
-            # convert windows style path string to unix style
-            path = path.replace('\\', '/')
+        path = os.path.normpath(path)
         if path:
-            img(cv2.imread(path))
+            # Fix for CJK path: read as bytes and decode
+            try:
+                with open(path, 'rb') as f:
+                    file_bytes = np.asarray(bytearray(f.read()), dtype=np.uint8)
+                    image = cv2.imdecode(file_bytes, getattr(cv2, flag, cv2.IMREAD_COLOR))
+                    img(image)
+            except Exception as e:
+                img(None)
